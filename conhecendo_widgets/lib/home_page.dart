e76cd7b1cbd8d418d8a2b26565408b709107510e
unknown <leonnardo.fer@gmail.com>
2022-04-19 22:22:37 -0300

    sincronizando o PageView com o BottomNivagationBar

--- conhecendo_widgets/lib/home_page.dart
+++ conhecendo_widgets/lib/home_page.dart
@@ -10,46 +10,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _pageController = PageController(initialPage: 0);
+  var indexBottomNavigationBar = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 80,
-            color: Colors.red,
-            child: const Center(
-              child: Text('Ola mundo',
-                  style: TextStyle(color: Colors.white, fontSize: 20.0)),
-            ),
-          ),
-          const SizedBox(height: 10), //Cria um espaçamento na coluna
-          Row(
-            // Cria uma linha de widgets
-            mainAxisAlignment:
-                MainAxisAlignment.center, //Centralizar o container ao centro
-            children: [
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) => {
+          setState(() => {indexBottomNavigationBar = index})
+        },
+        children: [
+          Column(
+            // Página 1
+            children: <Widget>[
               Container(
-                height: 80, // Altura
-                width: 80, // Largura
-                color: Colors.green, // Cor do Container
+                width: MediaQuery.of(context).size.width,
+                height: 80,
+                color: Colors.red,
+                child: const Center(
+                  child: Text('Ola mundo',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                ),
               ),
-              const SizedBox(width: 10), // Cria um espaço na linha
-              Container(
-                height: 80, // Altura
-                width: 80, // Largura
-                color: Colors.orangeAccent, // Cor do Container
+              const SizedBox(height: 10), //Cria um espaçamento na coluna
+              Row(
+                // Cria uma linha de widgets
+                mainAxisAlignment: MainAxisAlignment
+                    .center, //Centralizar o container ao centro
+                children: [
+                  Container(
+                    height: 80, // Altura
+                    width: 80, // Largura
+                    color: Colors.green, // Cor do Container
+                  ),
+                  const SizedBox(width: 10), // Cria um espaço na linha
+                  Container(
+                    height: 80, // Altura
+                    width: 80, // Largura
+                    color: Colors.orangeAccent, // Cor do Container
+                  ),
+                ],
               ),
             ],
           ),
+          Container(color: Colors.green), // Pagina 2
+          Container(color: Colors.blueAccent), // Página 3
         ],
       ),
+      // Barra de navegação
       bottomNavigationBar: BottomNavigationBar(
-        // Barra de navegação
+        onTap: (int page) => {
+          // Função Chamada quando um item é tocado
+          setState(() {
+            indexBottomNavigationBar = page;
+          }),
+          _pageController.animateToPage(page,
+              duration: const Duration(milliseconds: 400),
+              curve: Curves.easeInOut)
+        },
+        currentIndex: indexBottomNavigationBar
+            .toInt(), // Indica qual item da barra está selecionado
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
